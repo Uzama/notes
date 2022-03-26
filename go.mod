4a300d1de5b2ded2e5f5d58d02c17775cdf1a673
Uzama Zaid <uzamazaid@Uzamas-MacBook.local>
2022-03-26 12:04:48 +0530

    implemented struct validator

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module notes
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-playground/universal-translator v0.18.0
+	github.com/gorilla/mux v1.8.0
+	gopkg.in/go-playground/validator.v9 v9.31.0
+)
+
+require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
--- http/validators/validators.go
+++ http/validators/validators.go
@@ -1,1 +1,47 @@
 package validators
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	ut "github.com/go-playground/universal-translator"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type Validator struct {
+	validator  *validator.Validate
+	translator ut.Translator
+}
+
+func NewValidator() Validator {
+	validator := Validator{
+		validator: validator.New(),
+	}
+
+	return validator
+}
+
+func (val Validator) Validate(ctx context.Context, data interface{}) error {
+
+	err := val.validator.Struct(data)
+	if err == nil {
+		return nil
+	}
+
+	fieldErrors := err.(validator.ValidationErrors)
+
+	errMessage := ""
+
+	for i, e := range fieldErrors {
+		errMessage += fmt.Sprintf("param:%v is invalid. %s", e.Value(), e.Translate(val.translator))
+
+		if i == len(fieldErrors)-1 {
+			continue
+		}
+
+		errMessage += " | "
+	}
+
+	return errors.New(errMessage)
+}
